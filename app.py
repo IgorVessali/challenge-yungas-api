0e980452a958ea0b23b59e085f79b867be3bbbb2
Igor Vessali <igorvessali@gmail.com>
2021-03-06 14:07:35 -0300

    Implemented trainings in the routes

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 #===============================================================================================================================
 #   File with functions to start the project and create the routes
 #===============================================================================================================================
-
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template, abort, jsonify
 from database import formated_data
 from regions import list_regions
-from utils import paginate
+from utils import paginate, verifyPaginateParam
 
 peoples_db = formated_data()
 lst_regions = list_regions()
@@ -17,38 +16,45 @@ app = Flask(__name__)
 def index():
   return render_template('index.html') 
 
+
 # Creates the end-point that returns a list of all people
 # 'page_size'-'optional'-> number of items on the page
 # 'page'-'optional'-> current page
 @app.route('/peoples', methods=['GET'])
-def get_peoples(page_size = 20, page = 1):
-  if request.args.get('page_size'): page_size = int(request.args.get('page_size'))   
-  if request.args.get('page'): page = int(request.args.get('page')) 
+def get_peoples(page_size = 20, page = 1):  
+  if verifyPaginateParam(page_size, page):
+    return paginate(peoples_db, page_size, page), 200
+  else: abort(404, description="OOOPS!")
 
-  return paginate(peoples_db, page_size, page), 200
 
 # Creates the end-point that returns a list of people by region based on the state reported
 # 'page_size'-'optional'-> number of items on the page
 # 'page'-'optional'-> current page
 @app.route('/peoples/<region>', methods=['GET'])
-def get_peoples_per_region(region, page_size = 20, page = 1):  
-  if request.args.get('region'): 
-    return {"err": "The 'region' field is mandatory"}
-  if request.args.get('page_size'): page_size = int(request.args.get('page_size'))
-  if request.args.get('page'): page = int(request.args.get('page')) 
+def get_peoples_per_region(region, page_size = 20, page = 1): 
+  if region in lst_regions:
+    if verifyPaginateParam(page_size, page):
+      ret_peoples = [ people for people in peoples_db if people['location']['region'] == region ]
+      return paginate(ret_peoples, page_size, page), 200
+    else: abort(404, description="OOOPS!")
+  else: return abort(404, description="The reported region does not exist, see a list in http://127.0.0.1:5000/regions")
 
-  ret_peoples = [ people for people in peoples_db if people['location']['region'] == region ]
-  return paginate(ret_peoples, page_size, page), 200
   
 # Creates the end-point that returns a list of regions
 # 'page_size'-'optional'-> number of items on the page
 # 'page'-'optional'-> current page
 @app.route('/regions', methods=['GET'])
 def get_regions(page_size = 20, page = 1):
-  if request.args.get('page_size'): page_size = int(request.args.get('page_size'))   
-  if request.args.get('page'): page = int(request.args.get('page')) 
+  if verifyPaginateParam(page_size, page):
+    return paginate(lst_regions, page_size, page), 200
+  else: abort(404, description="OOOPS!")
 
-  return paginate(lst_regions, page_size, page), 200
+
+# Define a 404 return
+@app.errorhandler(404)
+def resource_not_found(e):
+    return jsonify(error=str(e)), 404
+  
 
 if __name__ == '__main__':
   app.run(debug=True)
--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@
 #   File with all shared functions
 #===============================================================================================================================
 import re
-from flask import json, Response
+from flask import json, Response, request
 #Formats the phone informed to the international standard
 # 'people'-'required'-> Person who will have their phone formatted 
 # 'nameItem'-'required'-> index of the list that the phone is
@@ -30,13 +30,17 @@ def removeItem(lst, nameItem):
 # 'date'-'required'-> the list that will be paged
 # 'page_size'-'required'->  number of items per page
 # 'page'-'required'->  of the current page
-def paginate(data, page_size, page): 
+def paginate(data, page_size, page):  
+  # if page_size < 1 : page_size = 20
+  # if page < 1 : page = 1
   if data and page_size and page:
     paginated = [ data [ i:i + page_size] for i in range(0, len(data), page_size)]
+
     # Valid if the reported page is valid if it does not redirect to the first page
     if len(paginated) <= page -1 or page < 1:
       page = 1
       print('The reported page does not exist, you will be redirected to the page 1.')
+
     #  Select the itens from the current page
     selected_itens = paginated[page -1]
     json_data = {"page": page,
@@ -48,3 +52,14 @@ def paginate(data, page_size, page):
     #creating a Response object to set the content type and the encoding
     response = Response(json_string, content_type="application/json; charset=utf-8" )
     return response
+
+# Checks if the passed parameters are valid
+# 'page_size'-'optional'->  number of items per page
+# 'page'-'optional'->  of the current page
+def verifyPaginateParam(page_size = 20, page = 1):
+  if request.args.get('page_size'): page_size = int(request.args.get('page_size'))   
+  if request.args.get('page'): page = int(request.args.get('page'))
+
+  if page > 0 and page_size > 0 :
+    return True
+  else: return False
